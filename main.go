ef58ba8d24e59f1f4b42ca72f58fe4a98eaf3b44
daegun han <runr4bbit@gmail.com>
2022-02-12 23:07:04 +0900

    wallets

--- main.go
+++ main.go
@@ -1,13 +1,11 @@
 package main
 
 import (
-	"github.com/DaegunHan/hancoin/blockchain"
 	"github.com/DaegunHan/hancoin/cli"
 	"github.com/DaegunHan/hancoin/db"
 )
 
 func main() {
 	defer db.Close()
-	blockchain.Blockchain()
 	cli.Start()
 }
--- rest/rest.go
+++ rest/rest.go
@@ -8,6 +8,7 @@ import (
 
 	"github.com/DaegunHan/hancoin/blockchain"
 	"github.com/DaegunHan/hancoin/utils"
+	"github.com/DaegunHan/hancoin/wallet"
 	"github.com/gorilla/mux"
 )
 
@@ -30,6 +31,10 @@ type urlDescription struct {
 type balanceResponse struct {
 	Address string `json:"address"`
 	Balance int    `json:"balance"`
+}
+
+type myWalletResponse struct {
+	Address string `json:"address"`
 }
 
 type errorResponse struct {
@@ -130,9 +135,16 @@ func transactions(rw http.ResponseWriter, r *http.Request) {
 	utils.HandleErr(json.NewDecoder(r.Body).Decode(&payload))
 	err := blockchain.Mempool.AddTx(payload.To, payload.Amount)
 	if err != nil {
-		json.NewEncoder(rw).Encode(errorResponse{"not enough funds"})
+		rw.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(rw).Encode(errorResponse{err.Error()})
+		return
 	}
 	rw.WriteHeader(http.StatusCreated)
+}
+
+func myWallet(rw http.ResponseWriter, r *http.Request) {
+	address := wallet.Wallet().Address
+	json.NewEncoder(rw).Encode(myWalletResponse{Address: address})
 }
 
 func Start(aPort int) {
@@ -144,7 +156,8 @@ func Start(aPort int) {
 	router.HandleFunc("/blocks", blocks).Methods("GET", "POST")
 	router.HandleFunc("/blocks/{hash:[a-f0-9]+}", block).Methods("GET")
 	router.HandleFunc("/balance/{address}", balance)
-	router.HandleFunc("/mempool", mempool)
+	router.HandleFunc("/mempool", mempool).Methods("GET")
+	router.HandleFunc("/wallet", myWallet).Methods("GET")
 	router.HandleFunc("/transactions", transactions).Methods("POST")
 	fmt.Printf("Listening on http://localhost%s\n", port)
 	log.Fatal(http.ListenAndServe(port, router))
